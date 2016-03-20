e426f1bdfa6a56f102422a0b4ace4816081d6cd9
Rachid <wikicenter-soft@yahoo.fr>
2016-03-20 22:12:32 +0100

    Test

---
+++ src/test/java/com/rac/PostServiceTest.java
@@ -0,0 +1,31 @@
+package com.rac;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.rac.config.AppConfig;
+import com.rac.service.PostService;
+
+@SpringApplicationConfiguration(classes = AppConfig.class)
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PostServiceTest {
+
+    @Autowired
+    private PostService postService;
+
+    @Test
+    public void TestConvertDate() {
+	Date currentDate = new Date();
+	SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-YYYY");
+	String dateConvertor = postService.convertDate(currentDate);
+	Assert.assertEquals(dateConvertor, dateFormat.format(currentDate));
+    }
+
+}
